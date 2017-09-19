6c535093285eaafd23eabb566e628845cca821ff
Oliver Meyer zu Altenschildesche <omza@live.de>
2017-09-19 09:54:11 +0200

    prepare dockerfile

--- otrrentetl/config/application/test.py
+++ otrrentetl/config/application/test.py
@@ -1,5 +1,5 @@
 # ottrent settings
-APPLICATION_PATH_LOG = 'C:/Users/omza/source/repos/otrrentserver/'
+APPLICATION_PATH_LOG = '/usr/log/'
 APPLICATION_LOGLEVEL_CONSOLE = 20 # Info 
 APPLICATION_LOGLEVEL_FILE = 20 # Info
 APPLICATION_LOG_LEVEL = 20 # Info
